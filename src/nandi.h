f302ce85fcfd9b316fb79b5356bb25ebafc6b22f
Piotr Szulakowski <pszulakowski321@gmail.com>
2024-03-12 16:40:19 +0100

    add string construction functions.

--- src/nandi.h
+++ src/nandi.h
@@ -3,11 +3,28 @@
 
 #include "../build/config.h"
 
-/* Nandi String */
-#ifndef NSTRING_IMPL
-typedef char* NString;
-#endif // NSTRING_IMPL
+/*************************
+Primitve types
+*************************/
+#include <stdint.h>
+typedef int8_t I8;
+typedef uint8_t U8;
+typedef int16_t I16;
+typedef uint16_t U16;
+typedef int32_t I32;
+typedef uint32_t U32;
+typedef int64_t I64;
+typedef uint64_t U64;
 
-NString nandi_string_new();
+/*************************
+Module: NString
+Source: nstring.c
+*************************/
+typedef char* NString;
+
+NString nandi_string_get_empty();
+NString nandi_string_get_from_cstring(char *cstring);
+
+/*************************/
 
 #endif //NANDI_H
--- src/nstring.c
+++ src/nstring.c
@@ -1,1 +1,27 @@
 #include "nandi.h"
+#include <stdlib.h>
+
+typedef struct {
+    U32 length;
+    char buffer[1];
+} *INStringWrapped;
+
+INStringWrapped nandi_string_wrapped_new(U32 length);
+INStringWrapped nandi_string_wrapped_new(U32 length) {
+    INStringWrapped stringWrapped = malloc(sizeof(*stringWrapped) + length);
+    stringWrapped->length = length;
+    stringWrapped->buffer[length] = 0;
+    return stringWrapped;
+}
+
+NString nandi_string_get_empty() {
+    INStringWrapped string;
+    string = nandi_string_wrapped_new(0);
+    return string->buffer;
+}
+
+NString nandi_string_get_from_cstring(char *cstring) {
+    INStringWrapped string;
+    string = nandi_string_wrapped_new(0);
+    return string->buffer;
+}
